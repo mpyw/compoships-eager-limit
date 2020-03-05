52d31c5cd6956614998447f7e284e4d4af411d3d
mpyw <ryosuke_i_628@yahoo.co.jp>
2020-03-05 14:33:21 +0900

    Revert "Chore: debugging"
    This reverts commit e896d9f01db3f8adc57ca39d4f85672b52de8385.

--- src/Database/Query/Grammar/SQLiteGrammar.php
+++ src/Database/Query/Grammar/SQLiteGrammar.php
@@ -7,20 +7,4 @@ use Staudenmeir\EloquentEagerLimit\Grammars\SQLiteGrammar as EagerLimitSQLiteGra
 class SQLiteGrammar extends EagerLimitSQLiteGrammar
 {
     use Concerns\CompilesGroupLimitByMultipleColumnPartition;
-
-    /**
-     * Compile a group limit clause.
-     *
-     * @param \Illuminate\Database\Query\Builder $query
-     * @return string
-     */
-    protected function compileGroupLimit(\Illuminate\Database\Query\Builder $query)
-    {
-        $version = $query->getConnection()->getPdo()->getAttribute(\PDO::ATTR_SERVER_VERSION);
-
-        // FIXME: For debugging
-        \Illuminate\Support\Facades\Log::channel('stderr')->debug("The PDO server version is: $version");
-
-        return parent::compileGroupLimit($query);
-    }
 }
